951ae7c07e31baeaa2db7670d6dbdaa3e567cb5c
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-01-28 15:29:54 -0800

    completed video 13

--- js/screens/play.js
+++ js/screens/play.js
@@ -16,6 +16,7 @@ game.PlayScreen = me.ScreenObject.extend({
 
 		/*binds the right key so when pressed it moves right*/
 		me.input.bindKey(me.input.KEY.RIGHT, "right");
+		me.input.bindKey(me.input.KEY.A, "attack");
 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
