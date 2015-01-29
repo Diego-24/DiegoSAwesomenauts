b984279bee3040bb2db4ddfb17276a4143b4895c
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-01-28 16:35:31 -0800

    completed video 15

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -15,6 +15,8 @@ game.PlayerEntity = me.Entity.extend({
 		}]);
 
 		this.body.setVelocity(5, 20);
+		/*keeps track of which direction your character is going*/
+		this.facing = "right";
 		/*the camera follows the player*/
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
@@ -32,17 +34,19 @@ game.PlayerEntity = me.Entity.extend({
 			/*setVelocity() and multiplying it by me.timer.tick.*/
 			/*me.timer.tick makes the movement look smooth*/
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.facing = "right";
 			/*flips the pictures of the character*/
 			this.flipX(true);
 		/*does everything as the 'right' if statement except it's doing it for 'left'*/
 		}else if(me.input.isKeyPressed("left")) {
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+			this.facing = "left";
 			this.flipX(false);
 		}else{
 			this.body.vel.x = 0;
 		}
 		/*allows you to jump by pressing up but not when you're falling or already jumping*/
-		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
+		if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling) {
 			this.jumping = true;
 			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		}
@@ -82,10 +86,29 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}
 
+		me.collision.check(this, true, this.collideHandler.bind(this), true);
 		this.body.update(delta);
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	collideHandler: function(response) {
+		if(response.b.type==='EnemyBaseEntity') {
+			var ydif = this.pos.y - response.b.pos.y;
+			var xdif = this.pos.x - response.b.pos.x;
+			
+			console.log("xdif " + xdif + " ydif " + ydif);
+
+			if(xdif>-35 && this.facing==='right' && (xdif<0)) {
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x -1;
+			}else if(xdif<70 && this.facing==='left' && xdif>0) {
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x +1;
+
+			}
+		}
 	}
 });
 
