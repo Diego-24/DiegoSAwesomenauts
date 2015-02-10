4f175804ad7a25a01511b53bc9cac4352029960f
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-02-10 12:24:55 -0800

    completed video 22
    creeps can hit player

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,7 +13,8 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
-
+		this.type = "PlayerEntity";
+		this.health = 20;
 		this.body.setVelocity(3, 10);
 		/*keeps track of which direction your character is going*/
 		this.facing = "right";
@@ -83,6 +84,11 @@ game.PlayerEntity = me.Entity.extend({
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	loseHealth: function(damage) {
+		this.health = this.health = damage;
+		console.log(this.health);
 	},
 
 	/*sets the collision between the tower and the player so you can't go through*/
@@ -282,6 +288,27 @@ game.EnemyCreep = me.Entity.extend({
 			this.pos.x = this.pos.x + 1;
 			/*checks that it has been at least 1 second since this creep hit a base*/
 			if((this.now-this.lastHit >= 1000)) {
+				/*updates the lastHit timer*/
+				this.lastHit = this.now;
+				/*makes the player call its loseHealth function and it a*/
+				/*damage of 1*/
+				response.b.loseHealth(1);
+			}
+		}else if (response.b.type==='PlayerEntity') {
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			
+			
+			
+			if(xdif>0) {
+				/*keeps moving the creep to the right to maintain its position*/
+				this.pos.x = this.pos.x + 1;
+				//this.lastAttacking=this.now;
+				this.body.vel.x = 0;
+			}
+			/*checks that it has been at least 1 second since this creep hit something*/
+			if((this.now-this.lastHit >= 1000) && xdif>0) {
 				/*updates the lastHit timer*/
 				this.lastHit = this.now;
 				/*makes the player call its loseHealth function and it a*/
