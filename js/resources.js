211483efe18fcec7017f0ed55f5cc84d7e4924ae
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-02-20 11:53:49 -0800

    added another song

--- js/resources.js
+++ js/resources.js
@@ -34,7 +34,7 @@ game.resources = [
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */
 	 /*loads the song*/
-	 {name: "8 bit superhero", type: "audio", src: "data/bgm/"},
+	 {name: "inspector norse", type: "audio", src: "data/bgm/"},
 
 	/* Sound effects. 
 	 * @example
