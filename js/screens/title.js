c3ab7931462c13c9a92464543743d6a2d90fc7e8
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-03-05 12:24:50 -0800

    completed video 40

--- js/screens/title.js
+++ js/screens/title.js
@@ -34,6 +34,30 @@ game.TitleScreen = me.ScreenObject.extend({
 				me.state.change(me.state.PLAY);
 			}
 		})));
+
+			me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				/*the settings of the text*/
+				this.font = new me.Font("Arial", 60, "gold");		
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+			},
+
+			/*Adds the text and sets the coordinates of the text*/
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt) {
+				return true;
+			},
+
+			newGame: function() {
+				me.input.releasePointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY);
+			}
+		})));
+
 	},
 	
 	
