63e144dc4b5df278281ee78c1b7d1c3a9d4f2252
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-04-02 20:06:13 -0700

    completed video 54

---
+++ php/model/config.php
@@ -0,0 +1,22 @@
+<?php
+	require_once(__DIR__ . "/database.php");
+	require_once(__DIR__ . "/date.php");
+	session_start();
+	session_regenerate_id(true);
+
+	/*Sets the path that leads to PHPBasics*/
+	$path = "/DiegoSAwesomenauts/php/";
+	
+	$host = "localhost";
+	$username = "root";
+	$password = "root";
+	$database = "awesomenauts_db";
+
+	/*checks if it has been set or not*/
+	if(!isset($_SESSION["connection"])) {
+		/*stores the object mysqli*/
+		$connection = new Database($host, $username, $password, $database);
+		/*assigns the connection variable to the session variable*/
+		$_SESSION["connection"] = $connection;
+	}
+	
