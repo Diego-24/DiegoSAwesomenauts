7aefa8acaf4cc49e6bc1d542a35fbe62eaed016b
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-04-02 20:42:02 -0700

    completed video 55

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -1,13 +1,6 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 
-	/*Creates the entire table*/
-	$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
-		. "id int(11) NOT NULL AUTO_INCREMENT,"
-		. "title varchar(255) NOT NULL,"
-		. "post text NOT NULL,"
-		. "PRIMARY KEY (id))");
-	
 	/*creates another table*/
 	$query = $_SESSION["connection"]->query("CREATE TABLE users("
 			. "id int(11) NOT NULL AUTO_INCREMENT,"
@@ -15,4 +8,9 @@
 			. "email varchar(50) NOT NULL,"
 			. "password char(128) NOT NULL,"
 			. "salt char(128) NOT NULL,"
+			. "exp int(4),"
+			. "exp1 int(4),"
+			. "exp2 int(4),"
+			. "exp3 int(4),"
+			. "exp4 int(4),"
 			. "PRIMARY KEY (id))");
