7f05e9db5a2a7c9bf934255158994b00681e78a7
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-04-07 12:22:49 -0700

    completed video 64

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,16 @@
 game.MiniMap = me.Entity.extend({
-	
+	init: function(x, y, settings) {
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap",
+			width: 225,
+			height: 127,
+			spritewidth: "225",
+			spriteheight: "127",
+			getShape: function() {
+				return (new me.Rect(0, 0, 225, 127)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+
+	}
 });
